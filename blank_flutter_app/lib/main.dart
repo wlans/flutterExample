c32f863a475f5c3554718ec4f40500dd4961b776
wlans <wyatt.lansdale@gmail.com>
2018-09-01 17:28:49 -0500

    start of dog app

--- blank_flutter_app/lib/main.dart
+++ blank_flutter_app/lib/main.dart
@@ -1,3 +1,4 @@
+// main.dart
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -5,9 +6,14 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /// MaterialApp is the base Widget for your Flutter Application
+    /// Gives us access to routing, context, and meta info functionality.
     return new MaterialApp(
-      title: 'Blank Flutter App',
-      home: new MyHomePage(title: 'Blank Flutter App'),
+      title: 'We Rate Dogs',
+      // Make all our text default to white
+      // and backgrounds default to dark
+      theme: new ThemeData(brightness: Brightness.dark),
+      home: new MyHomePage(title: 'We Rate Dogs'),
     );
   }
 }
@@ -24,21 +30,22 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    /// Scaffold is the base for a page.
+    /// It gives an AppBar for the top,
+    /// Space for the main body, bottom navigation, and more.
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Container(),
-      floatingActionButton: new Builder(builder: (context) {
-        return new FloatingActionButton(onPressed: () {
-          Scaffold.of(context).showSnackBar(
-                new SnackBar(
-                  backgroundColor: Colors.blue,
-                  content: new Text('SnackBar'),
-                ),
-              );
-        });
-      }),
-    );
+
+        /// App bar has a ton of functionality, but for now lets
+        /// just give it a color and a title.
+        appBar: new AppBar(
+          /// Access this widgets properties with 'widget'
+          title: new Text(widget.title),
+          backgroundColor: Colors.black87,
+        ),
+
+        /// Container is a convenience widget that lets us style it's
+        /// children. It doesn't take up any space itself, so it
+        /// can be used as a placeholder in your code.
+        body: new Container());
   }
 }
